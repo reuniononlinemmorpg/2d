88b0bcdbce758b9b825a0c51783dc024a212b2d3
igorsctt <igor.ss2020@hotmail.com>
2025-05-18 11:45:41 -0300

    feat(gallery):Added gallery section

---
+++ button.js
@@ -0,0 +1,16 @@
+const btnTopo = document.getElementById("btnTopo");
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+      btnTopo.style.display = "block";
+    } else {
+      btnTopo.style.display = "none";
+    }
+  };
+
+  btnTopo.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
